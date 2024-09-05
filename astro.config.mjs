64a569eb780f3a2829988dc55961b00ad8a1fa8d
DiegoT4l <lopez.diiego.107@hotmail.com>
2024-09-05 00:27:03 -0600

    chore: Update dependencies, component styles, and content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import robotsTxt from "astro-robots-txt";
+
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+  integrations: [tailwind(), robotsTxt()],
+  site: 'http://127.0.0.1:4321/'
 });
