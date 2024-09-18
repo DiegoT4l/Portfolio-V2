e3faa86527de680864b4490bd9a4cd03c853a173
DiegoT4l <lopez.diiego.107@hotmail.com>
2024-09-18 16:54:32 -0600

    chore: Update component styles and dependencies
    - Updated component styles and dependencies.
    - Added reference to .astro/types.d.ts in env.d.ts.
    - Updated paths in tsconfig.json to include @components/* and @icons/*.
    - Updated flowbite dependency to version 2.5.1 in package.json.
    - Updated stroke-width value in Comet.astro.
    - Updated import path for ExperienceItem in Experience.astro.
    - Deleted SkillsetItem.astro and Tooltip.astro.
    - Updated class values in About.astro.
    - Updated safelist in tailwind.config.mjs.
    - Updated import paths for Badge, SocialPill, BriefIcon, and CodeIcon in Hero.astro.
    - Added services.astro and Projects.astro.
    - Deleted components.astro, Skillset.astro, and components.astro.
    - Updated import paths for GitHub, Gulp, Tailwind, Link, LinkButton, Bootstrap, JQuery, Sass, Html, CSS, and JavaScript in Projects.astro.
    - Deleted components.astro in pages directory.
    - Updated import paths for Layout, SectionContainer, Experience, BriefIcon, CodeIcon, CheckIcon, Projects, Hero, TitleSection, and About in index.astro.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,11 +6,35 @@ export default {
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'./node_modules/flowbite/**/*.js'
 	],
+
 	darkMode: 'class',
+
+	safelist: [
+		'w-64',
+		'w-1/2',
+		'rounded-l-lg',
+		'rounded-r-lg',
+		'bg-gray-200',
+		'grid-cols-4',
+		'grid-cols-7',
+		'h-6',
+		'leading-6',
+		'h-9',
+		'leading-9',
+		'shadow-lg'
+	],
+
 	theme: {
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+		},
 		extend: {
 			fontFamily: {
-				"onest": ["Onest Variable", "sans-serif"],
+				onest: ["Onest Variable", "sans-serif"],
+				suse: ["SUSE Variable", "sans-serif"],
 			}
 		},
 	},
