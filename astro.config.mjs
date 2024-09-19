64886cfba36bc5f69a7d593953919ce4d9a703c6
lopez.diiego.107 <lopez.diiego.107@gmail.com>
2024-09-19 19:42:20 +0000

    feat: add Netlify deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from "astro-robots-txt";
 
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
   integrations: [tailwind(), robotsTxt()],
   site: 'http://127.0.0.1:4321/'
 });
